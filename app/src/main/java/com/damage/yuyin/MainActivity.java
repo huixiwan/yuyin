86df0d01b1864c6071ee6812ac4f397d8ee83836
huixiwan <2021787483@qq.com>
2020-08-16 16:57:33 +0800

    完善了情感语音输入的代码

--- app/src/main/java/com/damage/yuyin/MainActivity.java
+++ app/src/main/java/com/damage/yuyin/MainActivity.java
@@ -83,120 +83,14 @@ public class MainActivity extends AppCompatActivity {
                     showdialog(v);
                     break;
                 case R.id.btn_test2:
-                    test2();
+                    test();
                     break;
             }
         }
     }
 
     private void test() {
-        //Toast ts = Toast.makeText(getBaseContext(),"hahe",Toast.LENGTH_LONG);
-        //ts.show();
-        int position = 14;
-        position = houword(getTestString(),position);
-        System.out.println(position);
-        System.out.println(getTestString().substring(1,1));
-    }
 
-    private int houword(String string,int position){
-        boolean flag = true;
-        while(flag){
-            if(position == string.length()){
-                flag = false;
-            }else{
-                if(string.charAt(position)-'1'>=0 && string.charAt(position)-'1'<=4){
-                    flag = false;
-                    position++;
-                    System.out.println("是音节");
-                }else{
-                    position++;
-                    System.out.println("不是音节");
-                }
-            }
-        }
-        return position;
-    }
-
-    /*
-    private int qianword(String string,int position){
-        boolean flag = true;
-        while(flag){
-            if(position == 0){
-                flag = false;
-            }else{
-                if(string.charAt(position-1)-'1'>=0 && string.charAt(position-1)-'1'<=4){
-                    flag = false;
-                    System.out.println("是音节");
-                }else{
-                    position--;
-                    System.out.println("不是音节");
-                }
-            }
-        }
-        return position;
-    }
-
-     */
-
-    private MediaPlayer mMediaPlayerTest = null;
-    private int current = 0;
-    private void test2() {
-        System.out.println("test2 is ok!");
-        current = 0;
-        String[] testString = getStringArray();
-        Context context = getApplicationContext();
-        mMediaPlayerTest = MediaPlayer.create(context, getResId(testString[0], R.raw.class));
-        mMediaPlayerTest.start();
-        next();
-    }
-
-    private void next() {
-        mMediaPlayerTest.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
-            @Override
-            public void onCompletion(MediaPlayer mp) {
-                nextArr();
-            }
-        });
-    }
-
-    private void nextArr(){
-        String[] testString2 = getStringArray();
-        current=current+1;
-        Context context = getApplicationContext();
-        mMediaPlayerTest = MediaPlayer.create(context, getResId(testString2[current], R.raw.class));
-        mMediaPlayerTest.start();
-        if(current < testString2.length-1){
-            next();
-        }
-    };
-
-    private String[] getStringArray(){
-        String[] stringArray = getTestString().split(getTestReg());
-        String restString = getTestString();
-        for(int i = 0; i < stringArray.length-1; i++){
-            stringArray[i] = restString.substring(0,stringArray[i].length()+1);
-            restString = restString.substring(stringArray[i].length());
-        }
-        stringArray[stringArray.length-1] = restString;
-        return stringArray;
-    }
-
-    private String getTestString(){
-        return "chi1a1ou1ou2ou3ou4";
-    }
-
-    private String getTestReg() {
-        return "[1-5]";
-    }
-
-    public static int getResId(String variableName, Class<?> c) {
-        try {
-            Field idField = c.getDeclaredField(variableName);
-            return idField.getInt(idField);
-        } catch (Exception e) {
-            e.printStackTrace();
-            return -1;
-        }
     }
 
     private DialogInterface.OnClickListener click1=new DialogInterface.OnClickListener() {
